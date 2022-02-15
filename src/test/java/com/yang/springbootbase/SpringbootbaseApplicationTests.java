fc6329591a3f767730c7b8a6af530d347eed84de
zhangyang03 <zhangyang03@qianxin.com>
2022-02-15 20:54:56 +0800

    [M] 日志文件

--- src/test/java/com/yang/springbootbase/SpringbootbaseApplicationTests.java
+++ src/test/java/com/yang/springbootbase/SpringbootbaseApplicationTests.java
@@ -3,12 +3,16 @@ package com.yang.springbootbase;
 import com.yang.springbootbase.bean.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringbootbaseApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     Person person;
@@ -20,6 +24,12 @@ class SpringbootbaseApplicationTests {
     void contextLoads() {
         Assertions.assertNotNull(person);
         System.out.println(person);
+
+        logger.trace("---trace 日志---");
+        logger.debug("--- debug 日志---");
+        logger.info("--- info 日志---");
+        logger.warn("--- warn 日志---");
+        logger.error("--- error 日志---");
     }
 
     @Test
