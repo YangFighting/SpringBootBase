544664dce91b331cad1997772078fb1631b71b02
zhangyang03 <zhangyang03@qianxin.com>
2022-02-18 22:07:16 +0800

    【A】自定义starter

---
+++ hello-spring-boot-starter-autoconfigure/src/main/java/com/yang/hellospringbootstarterautoconfigure/bean/HelloProperties.java
@@ -0,0 +1,28 @@
+package com.yang.hellospringbootstarterautoconfigure.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+//@EnableConfigurationProperties
+@ConfigurationProperties("hello")
+public class HelloProperties {
+    private String prefix;
+    private String suffix;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
---
+++ hello-spring-boot-starter-autoconfigure/src/main/java/com/yang/hellospringbootstarterautoconfigure/service/HelloService.java
@@ -0,0 +1,17 @@
+package com.yang.hellospringbootstarterautoconfigure.service;
+
+import com.yang.hellospringbootstarterautoconfigure.bean.HelloProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 默认不要放在容器中
+ */
+public class HelloService {
+
+    @Autowired
+    private HelloProperties helloProperties;
+
+    public String sayHello(String userName){
+        return helloProperties.getPrefix() + ": " + userName + " > " + helloProperties.getSuffix();
+    }
+}
